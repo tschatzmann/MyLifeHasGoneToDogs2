e54398e1c08495b89b7668ba5246771c779cbf05
tschatzmann <tarrta2002@gmail.com>
2019-05-02 20:46:23 -0400

    working on posting page

--- create-react-express/client/src/Pages/postings.js
+++ create-react-express/client/src/Pages/postings.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios"
+import { Col, Row, Container } from "../components/grid";
+import { List, ListItem } from "../components/list";
+import { Input, TextArea, FormBtn } from "../components/form";
 
 
 class Postings extends Component {
     state = {
-        Postings: []
+        postings: []
     };
 
 
@@ -17,60 +20,46 @@ componentDidMount() {
   loadPostings = () => {
     console.log("at loadpostings")
     axios.get("/api/posting")
-    .then(response => console.log(response));
-      // .then(res => this.setState({ Postings: res.data }))
+  // .then(response => console.log(response));
+    .then(res => this.setState({ postings: res.data }))
   };
 
   render() {
     return (
-      // <Container fluid>
-      //   <Row>
-      //     <Col size="md-6">
-      //       <Jumbotron>
-      //         <h1>What Postings Should I Read?</h1>
-      //       </Jumbotron>
-      //       <form>
-      //         <Input name="title" placeholder="Title (required)" />
-      //         <Input name="author" placeholder="Author (required)" />
-      //         <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
-      //         <FormBtn>Submit Posting</FormBtn>
-      //       </form>
-      //     </Col>
-      //     <Col size="md-6 sm-12">
-      //       <Jumbotron>
-      //         <h1>Postings On My List</h1>
-      //       </Jumbotron>
-      //       {this.state.Postings.length ? (
-      //         <List>
-      //           {this.state.Postings.map(Posting => (
-      //             <ListItem key={Posting._id}>
-      //               <a href={"/Postings/" + Posting._id}>
-      //                 <strong>
-      //                   {Posting.title} by {Posting.author}
-      //                 </strong>
-      //               </a>
-      //               <DeleteBtn />
-      //             </ListItem>
-      //           ))}
-      //         </List>
-      //       ) : (
-      //         <h3>No Results to Display</h3>
-      //       )}
-      //     </Col>
-    //      </Container>
-    <main>
-<div>
-<h1>
-  welcome to posting page
-</h1>
-<div>
-  {/* {Postings} */}
-</div>
-</div>
-
-
-    </main>
-
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            {/* <Jumbotron> */}
+              <h1>What Postings Should I Read?</h1>
+            {/* </Jumbotron> */}
+            <form>
+              <TextArea name="text" placeholder="text" />
+              <FormBtn>Submit Posting</FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            {/* <Jumbotron> */}
+              <h1>Postings On My List</h1>
+            {/* </Jumbotron> */}
+            {this.state.postings.length ? (
+              <List>
+                {this.state.postings.map(posting => (
+                  <ListItem key={posting._id}>
+                    <a href={"/api/posting/" + posting._id}>
+                      <strong>
+                        {posting.text}
+                      </strong>
+                    </a>
+                    {/* <DeleteBtn /> */}
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+          </Row>
+         </Container>
     );
   }
 };
