2af13db634e71d53df76f955773810a82cbf7a45
tschatzmann <tarrta2002@gmail.com>
2019-05-21 15:00:25 -0400

    removing emoji

--- client/src/components/emoji/newspaper.js
+++ client/src/components/emoji/newspaper.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 export function NewspaperButton(props) {
   return (
          <button  style={{borderColor: "transparent", backgroundColor: "transparent"}} onClick={(e) => props.addUserReaction(e, props.allpost, props.emojiValue )} ><img className= "emoji-btn doghouse" style={{width: 40, height: 40, paddingLeft: -10}}src={require('./assets/images/dognewspaper.jpeg')} alt="newspaper"/></button>
